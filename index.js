74d48847d2c2d40b913db511544b049981f88f28
Daniil <den2007fad@gmail.com>
2024-12-11 19:57:33 +0300

    init

--- index.js
+++ index.js
@@ -1,36 +1,13 @@
 let countContainer = document.getElementById("counter");
-let click_btn = document.getElementById("click_btn");
 let reset_btn = document.getElementById("reset_btn");
-let input_countSum = document.getElementById("input_countSum");
 let boost_btn = document.getElementById("boost_btn");
-
-let sum_btn = document.getElementById("sum_btn");
 
 let count = 0;
 let isBoosted = false;
 
-click_btn.addEventListener("click", () => {
-  if (isBoosted) {
-    count += 10;
-  } else {
-    count++;
-  }
-
-  countContainer.textContent = count;
-  checkBoostAvailability();
-});
-
 reset_btn.addEventListener("click", () => {
   count = 0;
   isBoosted = false;
-  countContainer.textContent = count;
-  checkBoostAvailability();
-});
-
-sum_btn.addEventListener("click", () => {
-  const valueInput = parseInt(input_countSum.value) || 0;
-  count += valueInput;
-  input_countSum.value = "";
   countContainer.textContent = count;
   checkBoostAvailability();
 });
@@ -47,7 +24,6 @@ function checkBoostAvailability() {
     boost_btn.disabled = true;
   }
 }
-
 document
   .querySelector(".image__container img")
   .addEventListener("click", () => {
